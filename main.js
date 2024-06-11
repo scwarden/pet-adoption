b66706edc888a0ad884407701070987a853f8f50
Scott <scwarden1@gmail.com>
2024-06-11 15:32:48 -0400

    pet names to cards

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/109,51/forecast")
   const weatherData = await weatherPromise.json()
@@ -14,9 +17,14 @@ async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
 
   const petsData = await petsPromise.json()
-  petsData.foreach(() => {
-    console.log("hello")
+  petsData.forEach((pet) => {
+    const clone = template.content.cloneNode(true)
+
+    clone.querySelector("h3").textContent = pet.name
+
+    wrapper.appendChild(clone)
   })
+  document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
 petsArea()
