b974c98dd8a612659ff066b0b4fb3f79b3b54930
Scott <scwarden1@gmail.com>
2024-07-14 18:14:01 -0400

    finished pet filter

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ async function petsArea() {
   const petsData = await petsPromise.json()
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
+    clone.querySelector(".pet-card").dataset.species = pet.species
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
@@ -56,5 +57,14 @@ function handleButtonClick(e) {
 
   //add active class to just clicked
   e.target.classList.add("active")
+
   //actually filter
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    } else {
+      el.style.display = "none"
+    }
+  })
 }
